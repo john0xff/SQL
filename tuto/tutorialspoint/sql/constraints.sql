bd65e6183a2c73e41d9c9bc329e428fd6477d2f6
BartBien <phoenixjcam@gmail.com>
2014-10-07 22:13:42 +0200

    constraints update

--- tuto/tutorialspoint/sql/constraints.sql
+++ tuto/tutorialspoint/sql/constraints.sql
@@ -1,4 +1,7 @@
-/* ------------------------NOT NULL Constraint-------------------------------------------- Ensures that a column cannot have NULL value.*/
+/* http://www.tutorialspoint.com/sql/sql-rdbms-concepts.htm */
+
+/* ------------------------NOT NULL Constraint-----------------
+ *  Ensures that a column cannot have NULL value.*/
 CREATE TABLE CUSTOMERS(
        ID   INT              NOT NULL,
        NAME VARCHAR (20)     NOT NULL,
@@ -8,7 +11,8 @@ CREATE TABLE CUSTOMERS(
        PRIMARY KEY (ID)
 );
 
-/* ------------------------DEFAULT Constraint -------------------------------------------- Provides a default value for a column when none is specified.*/
+/* ------------------------DEFAULT Constraint ------------------
+ *  Provides a default value for a column when none is specified.*/
 CREATE TABLE CUSTOMERS(
        ID   INT              NOT NULL,
        NAME VARCHAR (20)     NOT NULL,
@@ -18,11 +22,14 @@ CREATE TABLE CUSTOMERS(
        PRIMARY KEY (ID)
 );
 
-ALTER TABLE CUSTOMERS MODIFY SALARY  DECIMAL (18, 2) DEFAULT 5000.00; /* If CUSTOMERS table has already been created, then to add a DFAULT constraint to SALARY column */
+/* If CUSTOMERS table has already been created, then to add a DFAULT constraint to SALARY column */
+ALTER TABLE CUSTOMERS MODIFY SALARY  DECIMAL (18, 2) DEFAULT 5000.00; 
 
-ALTER TABLE CUSTOMERS ALTER COLUMN SALARY DROP DEFAULT; /* To drop a DEFAULT constraint, use the following SQL: */
+/* To drop a DEFAULT constraint, use the following SQL: */
+ALTER TABLE CUSTOMERS ALTER COLUMN SALARY DROP DEFAULT; 
 
-/* ----------------------UNIQUE Constraint---------------------------------------------- Ensures that all values in a column are different. */
+/* ----------------------UNIQUE Constraint---------------------
+ * Ensures that all values in a column are different. */
 CREATE TABLE CUSTOMERS(
        ID   INT              NOT NULL,
        NAME VARCHAR (20)     NOT NULL,
@@ -31,5 +38,60 @@ CREATE TABLE CUSTOMERS(
        SALARY   DECIMAL (18, 2),       
        PRIMARY KEY (ID)
 );
+
+/*If CUSTOMERS table has already been created, then to add a UNIQUE constraint to AGE column*/
+ALTER TABLE CUSTOMERS MODIFY AGE INT NOT NULL UNIQUE;
+
+/*You can also use following syntax, which supports naming the constraint in multiple columns as well:*/
+ALTER TABLE CUSTOMERS ADD CONSTRAINT myUniqueConstraint UNIQUE(AGE, SALARY);
+
+/*To drop a UNIQUE constraint*/
+ALTER TABLE CUSTOMERS DROP CONSTRAINT myUniqueConstraint;
+
+/*If you are using MySQL, then you can use the following syntax:*/
+ALTER TABLE CUSTOMERS DROP INDEX myUniqueConstraint;
+
+/* ------------------Primary Key------------------ 
+ * Uniquely identified each rows/records in a database table. */
+CREATE TABLE CUSTOMERS(
+       ID   INT              NOT NULL,
+       NAME VARCHAR (20)     NOT NULL,
+       AGE  INT              NOT NULL,
+       ADDRESS  CHAR (25) ,
+       SALARY   DECIMAL (18, 2),       
+       PRIMARY KEY (ID)
+);
+
+/*To create a PRIMARY KEY constraint on the "ID" column when CUSTOMERS table already exists*/
+ALTER TABLE CUSTOMER ADD PRIMARY KEY (ID);
+
+/* ------------------multiple Primary Keys------------------ */
+/*For defining a PRIMARY KEY constraint on multiple columns*/
+CREATE TABLE CUSTOMERS(
+       ID   INT              NOT NULL,
+       NAME VARCHAR (20)     NOT NULL,
+       AGE  INT              NOT NULL,
+       ADDRESS  CHAR (25) ,
+       SALARY   DECIMAL (18, 2),        
+       PRIMARY KEY (ID, NAME)
+);
+
+/*To create a PRIMARY KEY constraint on the "ID" and "NAMES" columns when CUSTOMERS table already exists*/
+ALTER TABLE CUSTOMERS ADD CONSTRAINT PK_CUSTID PRIMARY KEY (ID, NAME);
+
+/*Delete Primary Key - clear the primary key constraints from the table*/
+ALTER TABLE CUSTOMERS DROP PRIMARY KEY ;
+
+/* -----------------  FOREIGN Key  ------------------- */
+/* Uniquely identified a rows/records in any another database table. */
+
+/**/
+
+/**/
+
+/**/
+
+/**/
+
 
 

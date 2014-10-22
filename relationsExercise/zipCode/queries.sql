1d72d4a445e79d456fba9b33aaacde1e3838b2c6
BartBien <phoenixjcam@gmail.com>
2014-10-22 08:43:42 +0200

    zipCode update

--- relationsExercise/zipCode/queries.sql
+++ relationsExercise/zipCode/queries.sql
@@ -1,3 +1,21 @@
 /* -------------------------------- */
 ALTER TABLE `student` ADD INDEX(`zipCodeID`);
+/* -------------------------------- */
+
+SELECT `student`.`name`, `zipcodes`.`zipCode`, `zipcodes`.`city`
+FROM `student`
+INNER JOIN `zipcodes`
+ON `student`.`zipcodeID` = `zipcodes`.`zipcodeID`
+
+/* -------------------------------- */
+
+SELECT `student`.`name` AS `studentName`, `zipcodes`.`zipCode`, `zipcodes`.`city`
+FROM `student`
+INNER JOIN `zipcodes`
+ON `student`.`zipcodeID` = `zipcodes`.`zipcodeID`
+
+/* -------------------------------- */
+
+
+
 

f4ac415d1797dedfb2abe61f9c2f4accd72d1edc
BartBien <phoenixjcam@gmail.com>
2014-11-06 22:11:50 +0100

    update GROUP BY

--- w3schools/additional/GROUP.sql
+++ w3schools/additional/GROUP.sql
@@ -1,5 +1,8 @@
 /* Aggregate functions often need an added GROUP BY statement. */
 /* The GROUP BY statement is used in conjunction with the aggregate functions to group the result-set by one or more columns. */
+/* ORDER BY alters the order in which items are returned.
+GROUP BY will aggregate records by the specified columns which allows you to 
+perform aggregation functions on non-grouped columns (such as SUM, COUNT, AVG, etc). */
 
 /* --------------------------------------------------- */
 SELECT Shippers.ShipperName,COUNT(Orders.OrderID) AS NumberOfOrders FROM Orders
